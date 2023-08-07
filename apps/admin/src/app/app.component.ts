42a0ff153b56e5af78ad955370c698435bbec85c
So Cool <127.0.0.1.pm@gmail.com>
2023-08-07 17:46:44 +0700

    fix broken admin component

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'badcompany-root',
   template: `
-    <badcompany-banner title="Welcome to our admin app."></badcompany-banner
+    <badcompany-banner title="Welcome to our admin app."></badcompany-banner>
   `
 })
 export class AppComponent {}
